47ed655adef6156e9cdf07d3a7d8993c09663f8d
brigit <brigit@DESKTOP-5DKQJUL.localdomain>
2026-07-31 00:52:47 +0300

    Complete login tracker lab

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  return (passwordAttempt) => {
+    attemptCount++;
 
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      return "Login successful";
+    }
+
+    if (passwordAttempt !== userInfo.password && attemptCount <= 3) {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+
+    return "Account locked due to too many failed login attempts";
+  };
+}
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
 };
